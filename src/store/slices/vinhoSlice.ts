d51fc569b9f61eeebfba1b3c9c715b8770158413
MarcoTullio <dev.marcotullio@gmail.com>
2024-06-30 20:33:30 -0300

    correção de caminho url redux

--- src/store/slices/vinhoSlice.ts
+++ src/store/slices/vinhoSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Vinho, Fornecedor } from '@/types/apiResponses';
+import { Vinho, Fornecedor } from '@/app/types/apiResponses';
 
 interface VinhoState {
     vinhos: Vinho[];
