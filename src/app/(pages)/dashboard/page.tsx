b0e7b3d3c843846f972f4b9a900f3be5f9af8516
MarcoTullio <dev.marcotullio@gmail.com>
2024-06-29 21:40:35 -0300

    login

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,13 +1,13 @@
 // src/app/page.tsx
 'use client'
 import ClientLayout from '@/app/client-layout';
-import AuthButton from '@/app/components/AuthButton';
+
 
 export default function Page() {
     return (
         <ClientLayout>
             <h1>Welcome to My Application</h1>
-            <AuthButton />
+
             {/* Outros componentes */}
         </ClientLayout>
     );
